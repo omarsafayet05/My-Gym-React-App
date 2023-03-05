aac60058c9f437bf9d2d5f3d6824b51ff6575cf7
omarsafayet05 <safayetkhan4431@gmail.com>
2023-03-05 08:46:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Card,
+  ButtonGroup,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Image,
+  Stack,
+  Heading,
+  Button,
+  Divider,
+  SimpleGrid,
+  Container,
+} from "@chakra-ui/react";
+
+import sample from "./image/bg3.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container maxW="container.lg">
+        <SimpleGrid
+          spacing={4}
+          templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
         >
-          Learn React
-        </a>
-      </header>
+          <Card bgGradient="linear(to-l, #7928CA, #FF0080)" maxW="sm">
+            <CardBody>
+              <Image
+                src={sample}
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Living room Sofa</Heading>
+                <Text>
+                  This sofa is perfect for modern tropical spaces, baroque
+                  inspired spaces, earthy toned spaces and for people who love a
+                  chic design with a sprinkle of vintage design.
+                </Text>
+                <Text color="blue.600" fontSize="2xl">
+                  $450
+                </Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Button variant="solid" colorScheme="blue">
+                  Buy now
+                </Button>
+                <Button variant="ghost" colorScheme="blue">
+                  Add to cart
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+          <Card maxW="sm" bgGradient="linear(to-l, #7928CA, #FF0080)">
+            <CardBody>
+              <Image
+                src={sample}
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Living room Sofa</Heading>
+                <Text>
+                  This sofa is perfect for modern tropical spaces, baroque
+                  inspired spaces, earthy toned spaces and for people who love a
+                  chic design with a sprinkle of vintage design.
+                </Text>
+                <Text color="blue.600" fontSize="2xl">
+                  $450
+                </Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Button variant="solid" colorScheme="blue">
+                  Buy now
+                </Button>
+                <Button variant="ghost" colorScheme="blue">
+                  Add to cart
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+          <Card maxW="sm" bgGradient="linear(to-l, #7928CA, #FF0080)">
+            <CardBody>
+              <Image
+                src={sample}
+                alt="Green double couch with wooden legs"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Living room Sofa</Heading>
+                <Text>
+                  This sofa is perfect for modern tropical spaces, baroque
+                  inspired spaces, earthy toned spaces and for people who love a
+                  chic design with a sprinkle of vintage design.
+                </Text>
+                <Text color="blue.600" fontSize="2xl">
+                  $450
+                </Text>
+              </Stack>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <ButtonGroup spacing="2">
+                <Button variant="solid" colorScheme="blue">
+                  Buy now
+                </Button>
+                <Button variant="ghost" colorScheme="blue">
+                  Add to cart
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+        </SimpleGrid>
+      </Container>
     </div>
   );
 }
